02760d29752c2b1febe9e503b5aab871d4e0bb02
kimminkyung05 <hhkim501@gmail.com>
2025-05-31 14:19:54 +0900

    Fix: use production backend URL

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f0184e9974cced07661ffc6b34eb7a82
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "af8d5590b12c5197a0721cee668f5a4d",
+"flutter_bootstrap.js": "423d99b6df619bc656fb45fc3fefccad",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
